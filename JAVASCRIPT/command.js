2c9195bb36189e1b8749457a7e491896f354d487
AnsonCheang50 <113309258+AnsonCheang50@users.noreply.github.com>
2022-11-01 19:07:11 -0400

    add
    adding in files

---
+++ JAVASCRIPT/command.js
@@ -0,0 +1,56 @@
+function MultiplicationTable()
+{
+    var hmin = document.getElementById("hmin").value;
+    var hmax = document.getElementById("hmax").value;
+    var vmin = document.getElementById("vmin").value;
+    var vmax = document.getElementById("vmax").value;
+    let t;
+    document.getElementById("wrong1").innerHTML = '';
+    document.getElementById("wrong2").innerHTML = '';
+    document.getElementById("wrong3").innerHTML = '';
+    document.getElementById("wrong4").innerHTML = '';
+    document.getElementById("graph").innerHTML = '';
+
+    if(hmin < -50 || hmin > 50 || hmin > hmax)
+    {
+        document.getElementById("wrong1").innerHTML = '<p> Value are not within range </p>';
+    }
+    else if(hmax < -50 || hmax > 50 || hmin > hmax)
+    {
+        document.getElementById("wrong2").innerHTML = '<p> Value are not within range </p>';
+    }
+    else if(vmin < -50 || vmin > 50 || vmin > vmax)
+    {
+        document.getElementById("wrong3").innerHTML = '<p> Value are not within range </p>';
+    }
+    else if(vmax < -50 || vmax > 50 || vmin > vmax)
+    {
+        document.getElementById("wrong4").innerHTML = '<p> Value are not within range </p>';
+    }
+    else
+    {
+        t = '<table border = 1px>';
+        t += '<tr>';
+        t += '<th id="topleft"> ' + ' ' + ' </th>';
+        for(var i = hmin; i <= hmax; i++)
+        {
+            t += '<th> ' + i + ' </th>';
+        }
+
+        t += '</tr>';
+
+        for(var i = vmin; i <= vmax; i++)
+        {
+            t += '<tr>';
+            t += '<th> ' + i + ' </th>';
+            for(var j = hmin; j <= hmax; j++)
+            {
+                t += '<th> ' + i * j + ' </th>';
+            }
+            t += '</tr>';
+        }
+
+        t += '</table>';
+        document.getElementById("graph").innerHTML = t;
+    }
+}
