3dd7feee48a31262d50b73c00ff3db5a318b0fc5
AnsonCheang50 <113309258+AnsonCheang50@users.noreply.github.com>
2022-11-01 19:26:06 -0400

    added description

--- JAVASCRIPT/command.js
+++ JAVASCRIPT/command.js
@@ -1,3 +1,12 @@
+/*
+    Creator : Anson Cheang
+    Contact : Anson0906@gmail.com
+    Source : TA, W3schools
+    Description : essentially checks 
+    for incorrect inputs, and create
+    the multiplication table
+*/
+
 function MultiplicationTable()
 {
     var hmin = document.getElementById("hmin").value;
@@ -5,12 +14,14 @@ function MultiplicationTable()
     var vmin = document.getElementById("vmin").value;
     var vmax = document.getElementById("vmax").value;
     let t;
+    //This clears the innerHTML making it so the table or error message is gone
     document.getElementById("wrong1").innerHTML = '';
     document.getElementById("wrong2").innerHTML = '';
     document.getElementById("wrong3").innerHTML = '';
     document.getElementById("wrong4").innerHTML = '';
     document.getElementById("graph").innerHTML = '';
 
+    //The if statements
     if(hmin < -50 || hmin > 50 || hmin > hmax)
     {
         document.getElementById("wrong1").innerHTML = '<p> Value are not within range </p>';
@@ -29,6 +40,7 @@ function MultiplicationTable()
     }
     else
     {
+        //the creation of the multiplication table
         t = '<table border = 1px>';
         t += '<tr>';
         t += '<th id="topleft"> ' + ' ' + ' </th>';
